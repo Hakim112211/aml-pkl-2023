ca880923d43b36e3b33f23010c22488c3b706a66
Hakim112211 <139119490+Hakim112211@users.noreply.github.com>
2023-07-11 16:28:26 +0700

    Add files via upload

---
+++ Day 4 (Function).js
@@ -0,0 +1,23 @@
+// Mengembalikan nama kalian
+
+function namakalian() {
+  const nama = 'hakim';
+  return nama;
+}
+
+console.log(namakalian());
+
+// Perkalian
+
+function kall(x, y) {- return x-*-y;-
+
+let basilkali kali(6, 2):- console.log("XXZ+ hasilkali)
+
+
+// Biodata
+
+function data_diri(nama, umur, alamat, hobby) {
+  console.log("Nama saya " + nama + ", umur saya " + umur + " tahun, alamat saya di " + alamat + ", dan saya punya hobby yaitu " + hobby + ".");
+}
+
+data_diri("Hakim", 17, "Bandung", "GYM");
